07576bd49ef8df9df6c6e86ae4bf0fa468bcee27
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-04 22:06:32 +0100

    5..10

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,6 +1,14 @@
-import Pricing from './4-pricing.js';
-import Currency from './3-currency.js';
+import Car from "./10-car";
 
-const p = new Pricing(100, new Currency("EUR", "Euro"))
-console.log(p);
-console.log(p.displayFullPrice());
+class TestCar extends Car {}
+
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+
+console.log(tc1 == tc2);
