37726ed520bfd8dafa510836d5dc7c32206524cd
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-04 21:41:17 +0100

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newamount) {
+    this._amount = newamount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newcurrency) {
+    this._currency = newcurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
