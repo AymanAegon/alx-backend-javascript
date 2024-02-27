9684a3e4a454dfd3d7821a4ef9d6d0bb272eb5ea
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-27 13:38:26 +0100

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (inp) => {
-  const input = inp.trim();
-  process.stdout.write(`Your name is: ${input}`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
