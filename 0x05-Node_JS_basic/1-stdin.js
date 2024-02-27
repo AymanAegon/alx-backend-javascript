9d1c87000934511f284c7830779ece650bac4745
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-27 13:14:27 +0100

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (inp) => {
   const input = inp.trim();
-  process.stdout.write(`Your name is: ${chunk}`);
+  process.stdout.write(`Your name is: ${input}`);
   process.exit();
 });
 
