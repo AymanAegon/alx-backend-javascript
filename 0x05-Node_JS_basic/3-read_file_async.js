6bff9b29fc4896c30ac1ea058ccf3f51eb5794b0
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-27 14:55:26 +0100

    3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,5 +1,38 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n');
+      lines.shift();
+
+      let studentsNumber = 0;
+      const fields = {};
+      lines.forEach((line) => {
+        if (line !== '') {
+        studentsNumber += 1;
+        const celles = line.split(',');
+        if (fields[celles[3]]) {
+            fields[celles[3]].push(celles[0]);
+        } else {
+            fields[celles[3]] = [celles[0]];
+        }
+        }
+      });
+      console.log(`Number of students: ${studentsNumber}`);
+      for (const field in fields) {
+        if (Object.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+      resolve();
+    });
+  });
   
 }
+
+module.exports = countStudents;
--- 0x05-Node_JS_basic/main_0.js
+++ 0x05-Node_JS_basic/main_0.js
@@ -1,6 +1,3 @@
-const countStudents = require('./2-read_file');
-
-countStudents("nope.csv");
 const countStudents = require('./3-read_file_async');
 
 countStudents("nope.csv")
