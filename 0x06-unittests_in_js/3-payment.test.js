1b3e1d74c8683a6c952b54ff4cbaae8220d94eb8
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-29 19:57:52 +0100

    3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,14 @@
 const { expect } = require('chai');
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./3-payment");
-const { calculateNumber } = require('./utils');
+const Utils = require('./utils')
 
 describe('sendPaymentRequestToApi', () => {
   it('checks function', () => {
-    const fun = sinon.spy(sendPaymentRequestToApi);
-    const original = sinon.spy(calculateNumber);
-    fun(100, 20);
-    original('SUM', 100, 20);
-    expect(fun.calledWith(100, 20)).to.equal(original.calledWith('SUM', 100, 20));
-    fun.restore();
-    original.restore();
+    const fun = sinon.spy(Utils);
+    sendPaymentRequestToApi(100, 20);
+    expect(fun.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(fun.calculateNumber.calledOnce).to.be.true;
+    fun.calculateNumber.restore();
   });
 });
--- 0x06-unittests_in_js/main.js
+++ 0x06-unittests_in_js/main.js
@@ -1,3 +1,5 @@
 const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require('./utils')
 
 console.log(sendPaymentRequestToApi(2, 3.6));
+console.log(Utils.calculateNumber('SUM', 2, 3.6));
