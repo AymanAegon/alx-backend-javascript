b35275e3767f9e816989aa0b00234d3bae39f18d
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-29 17:38:29 +0100

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,13 @@ const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function() {
   it("checks equality", function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+  it("checks equality", function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(1, 3.2), 4);
   });
   it("checks equality", function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
@@ -13,5 +19,8 @@ describe("calculateNumber", function() {
   });
   it("checks equality", function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it("checks equality", function() {
+    assert.strictEqual(calculateNumber(1.49, 3.7), 5);
   });
 });
